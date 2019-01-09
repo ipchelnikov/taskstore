baf460c0a7f643939b52f9988f6ae3871c9c204c
Igor Pchelnikov <gopchelnikov@gmail.com>
2019-01-09 16:47:55 +0100

    https://www.coderbyte.com/editor/guest:Longest%20Word:Cpp

--- C++/coder_byte_strings/main.cpp
+++ C++/coder_byte_strings/main.cpp
@@ -1,7 +1,14 @@
+// Challenge from https://www.coderbyte.com/editor/guest:Longest%20Word:Cpp
+//
+// Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
+// If there are two or more words that are the same length, return the first word from the string with that length.
+// Ignore punctuation and assume sen will not be empty.
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <regex>
 
 const std::string alphanum = "abcdefghijklmnopqrstuvwxyz"\
                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
@@ -25,10 +32,29 @@ std::string LongestWord(const std::string& sen) {
     return sen.substr(max->first, max->second);
 }
 
+std::string LongestWordReg(const std::string& sen) {
+
+    std::regex word_regex{"(\\w+)"};
+
+    size_t max_word_size = 0;
+    auto max_word = std::sregex_iterator();
+
+    for(auto i = std::sregex_iterator(sen.begin(), sen.end(), word_regex); i != std::sregex_iterator(); ++i) {
+        if ( i->str().size() > max_word_size )
+        {
+            max_word_size = i->str().size();
+            max_word = i;
+        }
+    }
+
+    return max_word->str();
+}
+
 int main() {
     // keep this function call here
     //cout << LongestWord(gets(stdin));
-    std::cout << LongestWord("a confusing /:sentence:/[ this is not!!!!!!!~");
+    //std::cout << LongestWord("a confusing /:sentence:/[ this is not!!!!!!!~");
+    std::cout << LongestWordReg("a confusing /:sentence:/[ this is not!!!!!!!~");
     return 0;
 
 }
