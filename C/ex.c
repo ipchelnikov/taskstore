b4e140d536b2b9096bb424ab942195b8bf5a0bd1
Igor Pchelnikov <gopchelnikov@gmail.com>
2019-01-09 10:29:16 +0100

    Typos in the comment

--- C/ex.c
+++ C/ex.c
@@ -5,8 +5,8 @@ void main()
   short a[5] = {1,2,3,4,5};
   char* b = a;
  
-  printf("%d %d \n", b[0], b[5]); // in case of littel endian (Linuz for example), the result will be 1 0
-                                  // short = 2 bytes, chat = 1 byte. 
+  printf("%d %d \n", b[0], b[5]); // in case of little endian (Linux for example), the result will be 1 0
+                                  // short = 2 bytes, char = 1 byte.
                                   // Printing the left part of the first element in the array
                                   // and the right part of third element
 }
