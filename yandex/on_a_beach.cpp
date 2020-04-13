862b953a5c780dfa1cd7474d5f98f3cf31f6ec12
Igor Pchelnikov <gopchelnikov@gmail.com>
2020-04-13 11:37:34 +0200

    Update on_a_beach.cpp

--- yandex/on_a_beach.cpp
+++ yandex/on_a_beach.cpp
@@ -2,39 +2,42 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
- 
-void test()
+
+// Task description: https://codeforces.com/gym/102558/problem/C
+
+constexpr auto MAX_DELTA = 10'000'000'000ul;
+
+void run_test()
 {
     int num;
     std::cin >> num;
- 
-    std::vector<int> v;
- 
+
+    std::vector<unsigned long> v;
+    v.reserve(num);
+
     for( auto i = 0; i < num; ++i )
     {
-        int a;
+        unsigned long a;
         std::cin >> a;
         v.push_back(a);
     }
- 
-    int min_delta = 10000000;
- 
+
+    unsigned long min_delta = MAX_DELTA;
+
     std::sort(v.begin(), v.end());
- 
+
     for( auto val = v.begin(); val != v.end()-1; ++val )
         min_delta = std::min(min_delta, *val^*(val+1) );
- 
+
     std::cout << min_delta << '\n';
 }
- 
- 
+
+
 int main()
 {
     int num_of_tests = 0;
     std::cin >> num_of_tests;
- 
+
     for(auto i = 0; i < num_of_tests; ++i)
-    {
-        test();
-    }
+        run_test();
 }
